54be4fad701ac089cef35c6365b42f1a39182339
kubus89 <jakub_89@wp.pl>
2018-02-03 15:25:03 +0100

    added Gif Model

--- src/main/java/pl/projekt/kwejk/controller/HomeController.java
+++ src/main/java/pl/projekt/kwejk/controller/HomeController.java
@@ -5,14 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.projekt.kwejk.model.Gif;
+import pl.projekt.kwejk.repository.GifRespository;
 
 
 @Controller
 public class HomeController {
 
+    private GifRespository gifRespository = new GifRespository();
+
     @GetMapping("/")
     public String hello(ModelMap modelMap){
-       modelMap.addAttribute("gif",new Gif("compiler-bot"));
+       modelMap.addAttribute("gifs",gifRespository.findAll());
         return "home";
     }
 }
