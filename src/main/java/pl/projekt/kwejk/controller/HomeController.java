26b817ac3b5cb36150a89d41a94424ceb5b2a14c
kubus89 <jakub_89@wp.pl>
2018-02-03 12:28:35 +0100

    added static files

--- src/main/java/pl/projekt/kwejk/controller/HomeController.java
+++ src/main/java/pl/projekt/kwejk/controller/HomeController.java
@@ -1,14 +1,15 @@
 package pl.projekt.kwejk.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String hello(){
-        return "hello";
+        return "home";
     }
 }
