c823839debc3de4e6e29071d544e90c0026337ff
kubus89 <jakub_89@wp.pl>
2018-02-03 13:51:52 +0100

    added Gif Model

--- src/main/java/pl/projekt/kwejk/controller/HomeController.java
+++ src/main/java/pl/projekt/kwejk/controller/HomeController.java
@@ -2,14 +2,17 @@ package pl.projekt.kwejk.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.projekt.kwejk.model.Gif;
 
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String hello(){
+    public String hello(ModelMap modelMap){
+       modelMap.addAttribute("gif",new Gif("compiler-bot"));
         return "home";
     }
 }
---
+++ src/main/java/pl/projekt/kwejk/model/Gif.java
@@ -0,0 +1,43 @@
+package pl.projekt.kwejk.model;
+
+public class Gif {
+
+    private String name;
+
+    private String username;
+
+    private boolean favorite;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public String getThymeleafFilePath(){
+        return"/gifs/"+getName()+".gif";
+    }
+    public Gif(String name){
+        this.name = name;
+    }
+
+
+}
