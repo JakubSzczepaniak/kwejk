a2d2af0f21593552947a4bb502feae070e6cdb35
kubus89 <jakub_89@wp.pl>
2018-02-03 15:37:57 +0100

    added Gif Model

--- src/main/java/pl/projekt/kwejk/repository/GifRespository.java
+++ src/main/java/pl/projekt/kwejk/repository/GifRespository.java
@@ -11,9 +11,9 @@ public class GifRespository {
 
     static {
         ALL_GIFS.add(new Gif("android-explosion"));
-        ALL_GIFS.add(new Gif("ben-mike"));
+        ALL_GIFS.add(new Gif("ben-and-mike"));
         ALL_GIFS.add(new Gif("book-dominos"));
-        ALL_GIFS.add(new Gif("compiler-hot"));
+        ALL_GIFS.add(new Gif("compiler-bot"));
         ALL_GIFS.add(new Gif("cowboy-coder"));
         ALL_GIFS.add(new Gif("infinite-andrew"));
 
